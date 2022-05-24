9a8ab2a69e784331acc43bbd7a6e3590256fa64a
Anjali Mavingal <anjalimavingal@gmail.com>
2022-05-25 03:23:00 +0530

    section 4 API changes committed

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -8,16 +8,16 @@ namespace Domain
     public class Activity
     {
         public Guid Id { get; set; }
-        public string? Title { get; set; }
+        public string Title { get; set; }
 
         public DateTime ActivityDate { get; set; }
-        public string? Description { get; set; }
+        public string Description { get; set; }
 
-        public string? Categoty { get; set; }
+        public string Categoty { get; set; }
 
-        public string? City { get; set; }
+        public string City { get; set; }
 
-        public string? Venue { get; set; }
+        public string Venue { get; set; }
 
     }
 }
